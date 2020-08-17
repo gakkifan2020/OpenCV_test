fac9b27f5d37455bb688b089914d97df2e66d3e3
gakkifan2020 <redmountainpass@sina.com>
2020-08-17 14:28:40 +0800

    baseline

--- split_ADT_pic_to_three.py
+++ split_ADT_pic_to_three.py
@@ -2,10 +2,9 @@ import OpenCV_spllit_down
 # import OpenCV_spllit_middle
 import OpenCV_spllit_up
 import os
-from concurrent.futures import ProcessPoolExecutor
 import multiprocessing
 
-which = 2 # 0:up  1: middle 2: down
+which = 0 # 0:up  1: middle 2: down
 
 path = './dataset/orign/'
 image_files = os.listdir(path)  # 扫描目标路径的文件,将文件名存入列表
@@ -15,7 +14,7 @@ a = 0
 
 if which == 0:
     for pic in image_files:
-        OpenCV_spllit_down.split_up( path + pic, a)
+        OpenCV_spllit_up.split_up( path + pic, a)
         a += 1
 
 if which == 2:
